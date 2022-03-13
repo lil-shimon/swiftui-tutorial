0bbff5ceb43135698059bde2491ab581935ee141
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-03-13 12:03:45 +0900

    Initial Commit

---
+++ swiftui-tutorial/swiftui_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_tutorialApp.swift
+//  swiftui-tutorial
+//
+//  Created by lil-shimon on 2022/03/13.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
