04bd06327bcbf81683fdf9ce9116eae99ed94c84
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-03-13 12:39:00 +0900

    feat: 🎸 impl mapkit

---
+++ swiftui-tutorial/MapView.swift
@@ -0,0 +1,26 @@
+//
+//  MapView.swift
+//  swiftui-tutorial
+//
+//  Created by lil-shimon on 2022/03/13.
+//
+
+import MapKit
+import SwiftUI
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
